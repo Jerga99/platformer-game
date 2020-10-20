56865811ac3dbb5d74581950ad26dc8d586663f7
Filip Jerga <jerga99@gmail.com>
2020-10-20 09:58:24 +0200

    Player update event

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -10,6 +10,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     scene.physics.add.existing(this);
 
     this.init();
+    this.initEvents();
   }
 
   init() {
@@ -21,8 +22,11 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     this.setCollideWorldBounds(true);
   }
 
-  preUpdate(time, delta) {
-    super.preUpdate(time, delta);
+  initEvents() {
+    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
+  }
+
+  update() {
     const { left, right } = this.cursors;
 
     if (left.isDown) {
