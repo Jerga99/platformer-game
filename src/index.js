5d6451227b69711615c99064165e0e567f6b7c7a
Filip Jerga <jerga99@gmail.com>
2020-10-21 10:13:20 +0200

    Setup camera

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ const MAP_WIDTH = 1600;
 
 const WIDTH = document.body.offsetWidth;
 const HEIGHT = 600;
-debugger
+
 const SHARED_CONFIG = {
   mapOffset: MAP_WIDTH > WIDTH ? MAP_WIDTH - WIDTH : 0,
   width: WIDTH,
-  height: HEIGHT
+  height: HEIGHT,
+  zoomFactor: 1.5
 }
 
 const Scenes = [PreloadScene, PlayScene];
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,8 +4,9 @@ import Player from '../entities/Player';
 
 class Play extends Phaser.Scene {
 
-  constructor() {
+  constructor(config) {
     super('PlayScene');
+    this.config = config;
   }
 
   create() {
@@ -18,6 +19,9 @@ class Play extends Phaser.Scene {
         platformsColliders: layers.platformsColliders
       }
     });
+
+
+    this.setupFollowupCameraOn(player);
   }
 
   createMap() {
@@ -44,6 +48,13 @@ class Play extends Phaser.Scene {
   createPlayerColliders(player, { colliders }) {
     player
       .addCollider(colliders.platformsColliders)
+  }
+
+  setupFollowupCameraOn(player) {
+    const { height, width, mapOffset, zoomFactor } = this.config;
+    this.physics.world.setBounds(0, 0, width + mapOffset, height + 200);
+    this.cameras.main.setBounds(0, 0, width + mapOffset, height).setZoom(zoomFactor);
+    this.cameras.main.startFollow(player);
   }
 }
 
