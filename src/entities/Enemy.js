7124e8d049185bcd85d0436d5970c4b87972ec41
Filip Jerga <jerga99@gmail.com>
2020-10-23 10:23:28 +0200

    Add raycast on the enemy

--- src/entities/Enemy.js
+++ src/entities/Enemy.js
@@ -20,6 +20,7 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
   init() {
     this.gravity = 500;
     this.speed = 150;
+    this.rayGraphics = this.scene.add.graphics({lineStyle: {width: 2, color: 0xaa00aa}});
 
     this.body.setGravityY(this.gravity);
     this.setSize(20, 45);
@@ -35,6 +36,22 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
 
   update(time, delta) {
     this.setVelocityX(30);
+    const { ray } = this.raycast(this.body);
+
+    this.rayGraphics.clear();
+    this.rayGraphics.strokeLineShape(ray);
+  }
+
+  raycast(body, raylength = 30) {
+    const { x, y, width, halfHeight } = body;
+    const line = new Phaser.Geom.Line();
+
+    line.x1 = x + width;
+    line.y1 = y + halfHeight;
+    line.x2 = line.x1 + raylength;
+    line.y2 = line.y1 + raylength;
+
+    return { ray: line };
   }
 }
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -32,34 +32,6 @@ class Play extends Phaser.Scene {
 
     this.createEndOfLevel(playerZones.end, player);
     this.setupFollowupCameraOn(player);
-
-    this.plotting = false;
-    this.graphics = this.add.graphics();
-    this.line = new Phaser.Geom.Line();
-    this.graphics.lineStyle(1, 0x00ff00);
-
-    this.input.on('pointerdown', this.startDrawing, this);
-    this.input.on('pointerup', pointer => this.finishDrawing(pointer, layers.platforms), this);
-  }
-
-  drawDebug(layer) {
-    const collidingTileColor = new Phaser.Display.Color(243, 134, 48, 255);
-    layer.renderDebug(this.graphics, {
-      tileColor: null,
-      collidingTileColor
-    })
-  }
-
-  startDrawing(pointer) {
-    if (this.tileHits && this.tileHits.length > 0) {
-      this.tileHits.forEach(tile => {
-        tile.index !== -1 && tile.setCollision(false)
-      })
-    }
-
-    this.line.x1 = pointer.worldX;
-    this.line.y1 = pointer.worldY;
-    this.plotting = true;
   }
 
   finishDrawing(pointer, layer) {
@@ -153,17 +125,6 @@ class Play extends Phaser.Scene {
       eolOverlap.active = false;
       console.log('Payer has won!');
     })
-  }
-
-  update() {
-    if (this.plotting) {
-      const pointer = this.input.activePointer;
-
-      this.line.x2 = pointer.worldX;
-      this.line.y2 = pointer.worldY;
-      this.graphics.clear();
-      this.graphics.strokeLineShape(this.line);
-    }
   }
 }
 
