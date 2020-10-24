faa4af9f618aa379bc85f45866a9e7f141da268d
Filip Jerga <jerga99@gmail.com>
2020-10-24 18:40:20 +0200

    Set timeout to set a velocity

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -72,12 +72,14 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
   bounceOff() {
     this.body.touching.right ?
-      this.setVelocity(-this.bounceVelocity, -this.bounceVelocity) :
-      this.setVelocity(this.bounceVelocity, -this.bounceVelocity);
+      this.setVelocityX(-this.bounceVelocity) :
+      this.setVelocityX(this.bounceVelocity);
+
+    setTimeout(() => this.setVelocityY(-this.bounceVelocity), 0);
   }
 
   takesHit(initiator) {
-    this.body.checkCollision.none = true;
+    if (this.hasBeenHit) { return; }
     this.hasBeenHit = true;
     this.bounceOff();
   }
