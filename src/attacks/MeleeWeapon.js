450faf00c00949ebfc1330f1823eef1a3da46551
Filip Jerga <jerga99@gmail.com>
2020-10-29 11:19:16 +0100

    Update hit position

--- src/attacks/MeleeWeapon.js
+++ src/attacks/MeleeWeapon.js
@@ -15,6 +15,7 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     this.wielder = null;
 
     this.setOrigin(0.5, 1);
+    this.setDepth(10);
 
     this.activateWeapon(false);
 
@@ -26,10 +27,24 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     })
   }
 
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (!this.active) { return; }
+
+    if (this.wielder.lastDirection === Phaser.Physics.Arcade.FACING_RIGHT) {
+      this.setFlipX(false);
+      this.body.reset(this.wielder.x + 15, this.wielder.y);
+    } else {
+      this.setFlipX(true);
+      this.body.reset(this.wielder.x - 15, this.wielder.y);
+    }
+
+  }
+
   swing(wielder) {
     this.wielder = wielder;
     this.activateWeapon(true);
-    this.body.reset(wielder.x, wielder.y);
     this.anims.play(this.weaponAnim, true);
 
   }
