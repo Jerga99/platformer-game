a8bedd8ea06e3006c82b161fbd21a92a3c0864b5
Filip Jerga <jerga99@gmail.com>
2020-10-29 12:04:22 +0100

    delay the attack

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -6,6 +6,7 @@ import collidable from '../mixins/collidable';
 import anims from '../mixins/anims';
 import Projectiles from '../attacks/Projectiles';
 import MeleeWeapon from '../attacks/MeleeWeapon';
+import { getTimestamp } from '../utils/functions';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
@@ -34,6 +35,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     this.lastDirection = Phaser.Physics.Arcade.FACING_RIGHT;
     this.projectiles = new Projectiles(this.scene);
     this.meleeWeapon = new MeleeWeapon(this.scene, 0, 0, 'sword-default');
+    this.timeFromLastSwing = null;
 
     this.health = 100;
     this.hp = new HealthBar(
@@ -57,8 +59,15 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     })
 
     this.scene.input.keyboard.on('keydown-E', () => {
+
+      if (this.timeFromLastSwing &&
+          this.timeFromLastSwing + this.meleeWeapon.attackSpeed > getTimestamp()) {
+            return;
+      }
+
       this.play('throw', true);
       this.meleeWeapon.swing(this);
+      this.timeFromLastSwing = getTimestamp();
     })
   }
 
