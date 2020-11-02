650a0ce40e87b774d85b9b4f761572448df4e647
Filip Jerga <jerga99@gmail.com>
2020-11-02 12:06:06 +0100

    Add image to container

--- src/hud/index.js
+++ src/hud/index.js
@@ -9,26 +9,35 @@ class Hud extends Phaser.GameObjects.Container {
     scene.add.existing(this)
 
     const { rightTopCorner } = scene.config;
-    this.setPosition(rightTopCorner.x - 75, rightTopCorner.y + 5);
+
+    this.containerWidth = 70;
+    this.setPosition(rightTopCorner.x - this.containerWidth, rightTopCorner.y + 10);
     this.setScrollFactor(0);
 
+    this.fontSize = 20;
     this.setupList();
   }
 
   setupList() {
-    this.fontSize = 20;
-    const scoreBoard = this.scene.add.text(0, 0, '0', {fontSize: `${this.fontSize}px`, fill: '#fff'});
-    const scoreBoard2 = this.scene.add.text(0, 0, 'Hello', {fontSize: `${this.fontSize}px`, fill: '#fff'});
-    const scoreBoard3 = this.scene.add.text(0, 0, 'Hello 2', {fontSize: `${this.fontSize}px`, fill: '#fff'});
+    const scoreBoard = this.createScoreboard();
 
-    this.add([scoreBoard, scoreBoard2, scoreBoard3]);
+    this.add([scoreBoard]);
 
     let lineHeight = 0;
     this.list.forEach(item => {
       item.setPosition(item.x, item.y + lineHeight);
       lineHeight += 20;
     })
+  }
 
+  createScoreboard() {
+    const scoreText = this.scene.add.text(0, 0, '0', {fontSize: `${this.fontSize}px`, fill: '#fff'});
+    const scoreImage = this.scene.add.image(scoreText.width + 5, 0, 'diamond')
+      .setOrigin(0)
+      .setScale(1.3);
+
+    const scoreBoard = this.scene.add.container(0,0, [scoreText, scoreImage]);
+    return scoreBoard
   }
 }
 
